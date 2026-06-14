88f0100cf9c77ef062110f368363b932d92efb4e
AnutikLutik228 <ao1802275@gmail.com>
2026-06-14 16:27:25 +0300

    Добавлено создание данных о горах через Docker volume

--- generator/generate.py
+++ generator/generate.py
@@ -1,1 +1,19 @@
-print("generator works")
+import csv, random
+
+mountains = {"Everest": "Nepal", "Elbrus": "Russia", "Mont Blanc": "France", "Kilimanjaro": "Tanzania", "Denali": "USA"}
+
+d = ["easy", "medium", "hard"]
+
+with open("/app/data/data.csv", "w", newline="") as f:
+    w = csv.writer(f)
+
+    w.writerow(["id", "mountain", "country", "height", "difficulty"])
+
+    names = list(mountains.keys())
+
+    for i in range(10):
+        mountain = random.choice(names)
+
+        w.writerow([i + 1, mountain, mountains[mountain], random.randint(1000, 9000), random.choice(d)])
+
+print("mountain csv created")
